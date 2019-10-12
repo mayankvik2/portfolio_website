c75dc8377c37230e1e218f794cfda608e50cfc8f
Mayank Vikram <mayank.vikram@Indegene.com>
2019-10-12 22:56:43 +0530

    add postgresql database in working status checked through admin, also added Blog module

--- portfolio1/settings.py
+++ portfolio1/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'jobs.apps.JobsConfig',
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'portfolio1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD':'vikram',
+        'HOST':'127.0.0.1',
+        'PORT':'5432',
     }
 }
 
