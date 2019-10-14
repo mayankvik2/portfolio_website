2aa50c07ea763bc1f920847ed2f328e0349ea395
Mayank Vikram <mayank.vikram@Indegene.com>
2019-10-14 08:13:37 +0530

    Final Working Code

--- portfolio1/urls.py
+++ portfolio1/urls.py
@@ -4,8 +4,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 import jobs.views
+import blog.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', jobs.views.home, name='home'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('blog/',include('blog.urls'))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
